bcce402b7804a84c27724ff85c374e48f116676f
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-02-25 17:23:48 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,6 +17,6 @@ def get_google_new_results( term, count ):
 
     return results
 
-items = get_google_new_results( 'cádiz', 10 )
+items = get_google_new_results( 'cadiz', 7 )
 for i,e in enumerate(items):
     print '%d: %s' % (i+1,e,)
